4663d3b27e97879ddbec665776f71b048aa01768
Myles Selim <Selim042@users.noreply.github.com>
2018-07-03 19:43:16 -0500

    Version bump

--- src/main/java/selim/geyserrecipes/shared/GeyserRecipesInfo.java
+++ src/main/java/selim/geyserrecipes/shared/GeyserRecipesInfo.java
@@ -4,7 +4,7 @@ public class GeyserRecipesInfo {
 
 	public static final String ID = "geyserrecipes";
 	public static final String NAME = "Geyser Recipes";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "1.0.0";
 	public static final String CHANNEL = "geyser_recipes";
 	public static final String SHAPED_RECIPE = "geyser_recipes:shaped";
 	public static final String SHAPELESS_RECIPE = "geyser_recipes:shapeless";
